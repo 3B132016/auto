071b16087759cd99acaa968c2d7c84fb00b4df43
TsengJay <tsengjay00@>
2024-10-27 21:32:58 +0800

    all check

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    public function __construct()
+    {
+        echo 'Hello Lara~' . PHP_EOL;
+    }
+}
---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,10 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    public function __construct($name)
+    {
+        echo 'Hello ' . $name . PHP_EOL;
+    }
+}
