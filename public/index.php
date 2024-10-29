ac75ee3a593123e25c9e8120d747749479cd7fb7
TsengJay <tsengjay00@>
2024-10-29 21:17:55 +0800

    add log folder and edit index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,19 @@
 <?php
 require '../vendor/autoload.php';
 
-// 類別先 import 進來
+// 引入需要的類別
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
+use Demo\HelloWorld as World;
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
+// 創建 Monolog 的 log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+
+// 使用 HelloWorld 和其他類別
 $lara = new Lara();
 $vincent = new Someone('Vincent');
 
@@ -13,6 +22,9 @@ $mary = new \Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
 $hello = new Demo\HelloWorld();
 
-// 類別另取別名
-use Demo\HelloWorld as World;
+// 使用 World 別名
 $world = new World();
+
+// 添加記錄到 log
+$log->warning('Foo');
+$log->error('Bar');
